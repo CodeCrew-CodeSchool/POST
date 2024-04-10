bde3856c463c075d35586d79813d2a2a8ff9f727
DJ <epicmrjones@gmail.com>
2024-04-10 15:45:00 -0500

    img updated

--- post/src/Mon.js
+++ post/src/Mon.js
@@ -2,7 +2,7 @@ function Mon(props) {
     return (
         <div>
             <h3>{props.title}</h3>
-            <img src={props.image_url} alt=""/>
+            <img src={props.image_url} width="300px" alt=""/>
             <p>{props.description}</p>
         </div>
     )
